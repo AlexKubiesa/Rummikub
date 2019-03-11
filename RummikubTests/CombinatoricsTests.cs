e5f3480cacc8eccfa9be33b3bd3161a22e037f03
Alex Kubiesa <akubiesa@btinternet.com>
2019-03-11 22:46:28 +0000

    Fix tests.

--- RummikubTests/CombinatoricsTests.cs
+++ RummikubTests/CombinatoricsTests.cs
@@ -9,35 +9,40 @@ namespace RummikubTests
     [TestFixture]
     public class CombinatoricsTests
     {
-        public static IEnumerable<TestCaseData> GetSubsetsTestCases = new[]
+        public static IEnumerable<TestCaseData> GetSublistsTestCases = new[]
         {
             new TestCaseData(
                     new int[0],
                     new[] { new int[0] })
-                .SetName("Empty set"),
+                .SetName("Empty list"),
 
             new TestCaseData(
                     new[] { 1 },
                     new[] { new int[0], new[] { 1 } })
-                .SetName("Set of size 1"),
+                .SetName("List of size 1"),
 
             new TestCaseData(
                     new[] { 1, 2 },
                     new[] { new int[0], new[] { 1 }, new[] { 2 }, new[] { 1, 2 } })
-                .SetName("Set of size 2"),
+                .SetName("List of size 2"),
+
+            new TestCaseData(
+                    new[] { 1, 1 },
+                    new[] { new int[0], new[] { 1 }, new[] { 1 }, new[] { 1, 1 } })
+                .SetName("List with duplicate element"),
 
             new TestCaseData(
                     new[] { 1, 2, 3 },
                     new[] { new int[0], new[] { 1 }, new[] { 2 }, new[] { 3 },
                         new[] { 1, 2 }, new[] { 2, 3 }, new[] { 1, 3 }, new[] { 1, 2, 3 } })
-                .SetName("Set of size 3")
+                .SetName("List of size 3")
         };
 
         [Test]
-        [TestCaseSource(nameof(GetSubsetsTestCases))]
-        public void TestGetSubsets(IEnumerable<int> set, IEnumerable<IEnumerable<int>> expectedSubsets)
+        [TestCaseSource(nameof(GetSublistsTestCases))]
+        public void TestGetSubsets(IReadOnlyList<int> list, IEnumerable<IEnumerable<int>> expectedSubsets)
         {
-            var actualSubsets = set.GetSublists();
+            var actualSubsets = list.GetSublists();
             Assert.That(actualSubsets, Is.EquivalentTo(expectedSubsets).Using(new CollectionEqualityComparer<int>()));
         }
     }
