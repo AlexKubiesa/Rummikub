19dc848fec59f329226e12609e172a06d246c3f8
Alex Kubiesa <akubiesa@btinternet.com>
2019-03-12 23:08:29 +0000

    Add another test.

---
+++ RummikubTests/Scoring/CombinationSamplingScoreIntervalCalculatorTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using RummikubLib.Game;
+using RummikubLib.Scoring;
+
+namespace RummikubTests.Scoring
+{
+    [TestFixture]
+    public class CombinationSamplingScoreIntervalCalculatorTests
+    {
+        IScoreIntervalCalculator calculator;
+
+        [SetUp]
+        public void SetUp()
+        {
+            calculator = CombinationSamplingScoreIntervalCalculator.Instance;
+        }
+
+        [Test]
+        public void CheckResultsAreCorrect()
+        {
+            var tiles = new[]
+            {
+                Tile.CreateNumberedTile(TileColor.Red, 5),
+                Tile.CreateNumberedTile(TileColor.Red, 6),
+                Tile.CreateNumberedTile(TileColor.Red, 7),
+                Tile.CreateNumberedTile(TileColor.Red, 8),
+                Tile.CreateNumberedTile(TileColor.Red, 9),
+                Tile.CreateNumberedTile(TileColor.Red, 10),
+                Tile.CreateNumberedTile(TileColor.Red, 11),
+                Tile.CreateNumberedTile(TileColor.Blue, 8),
+                Tile.CreateNumberedTile(TileColor.Black, 8),
+                Tile.CreateNumberedTile(TileColor.Blue, 6),
+                Tile.CreateNumberedTile(TileColor.Black, 6),
+                Tile.CreateNumberedTile(TileColor.Red, 10),
+
+            };
+
+            var scoreInterval = calculator.GetScoreInterval(tiles);
+            Assert.That(scoreInterval.Min, Is.AtLeast(30));
+        }
+    }
+}
