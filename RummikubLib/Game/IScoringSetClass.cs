7e1452774aa60781e490332808c8607a6f53ffd9
Alex Kubiesa <akubiesa@btinternet.com>
2019-03-20 22:54:12 +0000

    Make a new class for equivalence classes of runs.

---
+++ RummikubLib/Game/IScoringSetClass.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace RummikubLib.Game
+{
+    public interface IScoringSetClass : IReadOnlyCollection<ITileClass>
+    {
+        ScoringSetType Type { get; }
+
+        int GetScore();
+    }
+}
---
+++ RummikubLib/Game/RunClass.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RummikubLib.Game
+{
+    class RunClass : IScoringSetClass
+    {
+        readonly ITileClass[] tiles;
+
+        public RunClass(TileColor color, int startValue, int endValue)
+        {
+            if (color == TileColor.None)
+            {
+                throw new ArgumentOutOfRangeException(nameof(color));
+            }
+
+            if (startValue < 1)
+            {
+                throw new ArgumentOutOfRangeException(nameof(startValue));
+            }
+
+            if (endValue > 13)
+            {
+                throw new ArgumentOutOfRangeException(nameof(endValue));
+            }
+
+            if (startValue > endValue)
+            {
+                throw new ArgumentException("Start value cannot be greater than end value.");
+            }
+
+            int length = endValue - startValue + 1;
+
+            if (length < 3)
+            {
+                throw new ArgumentException("A run must be of length at least 3.");
+            }
+
+            tiles = Enumerable.Range(startValue, length)
+                .Select(value => TileClass.CreateNumberedTileClass(color, value))
+                .ToArray();
+        }
+
+        public int Count => tiles.Length;
+
+        public ScoringSetType Type => ScoringSetType.Run;
+
+        public IEnumerator<ITileClass> GetEnumerator()
+        {
+            return ((IEnumerable<ITileClass>)tiles).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public int GetScore()
+        {
+            return tiles.Sum(x => x.Value);
+        }
+    }
+}
