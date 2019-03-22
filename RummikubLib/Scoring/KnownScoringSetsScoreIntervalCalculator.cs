33c18239ca9563b444eb62a4172e703895032671
Alex Kubiesa <akubiesa@btinternet.com>
2019-03-22 10:41:37 +0000

    Update one of the score interval calculators.

--- RummikubLib/Scoring/KnownScoringSetsScoreIntervalCalculator.cs
+++ RummikubLib/Scoring/KnownScoringSetsScoreIntervalCalculator.cs
@@ -14,11 +14,11 @@ namespace RummikubLib.Scoring
 
         public Range GetScoreInterval(IReadOnlyCollection<ITile> tiles)
         {
-            foreach (var scoringSet in HighScoringSetClasses.ScoringSetClasses)
+            foreach (var scoringSetClass in HighScoringSetClasses.ScoringSetClasses)
             {
-                if (scoringSet.Tiles.All(t => tiles.Contains(t, TileValueEqualityComparer.Instance)))
+                if (scoringSetClass.All(tileClass => tiles.Any(tile => tile.Class.Equals(tileClass))))
                 {
-                    return new Range(scoringSet.GetScore(), double.PositiveInfinity);
+                    return new Range(scoringSetClass.GetScore(), double.PositiveInfinity);
                 }
             }
 
